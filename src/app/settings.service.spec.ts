02a32f1c68582961441024166a27f7f745fc95d3
Andreas Tennert <mail@andreas-tennert.de>
2021-09-17 20:02:05 +0200

    feat: store word provider setting

--- src/app/settings.service.spec.ts
+++ src/app/settings.service.spec.ts
@@ -10,12 +10,24 @@ describe('SettingsService', () => {
     service = TestBed.inject(SettingsService);
   });
 
+  afterEach(() => {
+    localStorage.removeItem('wordProvider');
+  });
+
   it('should return the simple word provider setting as default', () => {
+    console.log(service.getWordProvider());
     expect(service.getWordProvider()).toEqual(WordProvider.SIMPLE);
   });
 
   it('should allow to change the word provider setting', () => {
     service.setWordProvider(WordProvider.KRAUT_IPSUM);
+    expect(service.getWordProvider()).toEqual(WordProvider.KRAUT_IPSUM);
+  });
+
+  it('should persist between sessions', () => {
+    let service = new SettingsService();
+    service.setWordProvider(WordProvider.KRAUT_IPSUM);
+    service = new SettingsService()
     expect(service.getWordProvider()).toEqual(WordProvider.KRAUT_IPSUM);
   });
 });
--- src/app/settings.service.ts
+++ src/app/settings.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 export enum WordProvider {
   SIMPLE,
@@ -15,13 +15,30 @@ export class SettingsService {
 
   private wordProvider = WordProvider.SIMPLE;
 
-  constructor() { }
+  constructor() {
+    this.loadSettings()
+  }
 
   public setWordProvider(wordProvider: WordProvider) {
     this.wordProvider = wordProvider;
+    localStorage.setItem('wordProvider', `${wordProvider}`);
   }
 
   public getWordProvider(): WordProvider {
     return this.wordProvider;
+  }
+
+  private loadSettings() {
+    // @ts-ignore
+    this.wordProvider = SettingsService.loadSetting<WordProvider>('wordProvider', WordProvider.SIMPLE, v => WordProvider[WordProvider[parseInt(v, 10)]]);
+  }
+
+  private static loadSetting<T>(key: string, defaultValue: T, converter: (a: string) => T): T {
+    const storageValue = localStorage.getItem(key);
+    if (storageValue != null) {
+      return converter(storageValue);
+    } else {
+      return defaultValue;
+    }
   }
 }
